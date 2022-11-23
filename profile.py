d2312dd6907467bdb9bbbc1a528b454da9b2f163
AxolPop <derfii2004@gmail.com>
2022-11-23 18:51:57 +0000

    Made it easier to edit profiles

--- profile.py
+++ profile.py
@@ -1,66 +1,96 @@
-import interactions
 import database_manager as db
 import Badges.stamp_viewer as view
 
-class Profile(interactions.Extension):
-    
-    profile_background_choices = [
-        interactions.Choice(
-            name='Normal',
-            value='Normal'
-        ),
-        interactions.Choice(
-            name='Red',
-            value='Red'
-        ),
-        interactions.Choice(
-            name='Blue',
-            value='Blue'
-        ),
-        interactions.Choice(
-            name='Green',
-            value='Green'
-        ),
-        interactions.Choice(
-            name='Yellow',
-            value='Yellow'
-        ),
-        interactions.Choice(
-            name='Pink',
-            value='Pink'
-        ),
-    ]
+from interactions import *
+from interactions.ext.wait_for import *
+
+class Profile(Extension):
     
     def __init__(self, client):
         print('Profile settings loaded!')
         
-    @interactions.extension_command()
+    def GetValues(index):
+        values = []
+        
+    @extension_command()
     async def profile(self, ctx):
         pass
     
-    @profile.subcommand(description='Change your profile background.')
-    @interactions.option(description='The profile background to set to.', choices = profile_background_choices, required = True)
-    async def set_background(self, ctx : interactions.CommandContext, choice : str = 'Normal'):
-        id_ = int(ctx.author.id)
+    @profile.subcommand(description='Edit your profile.')
+    async def edit(self, ctx : CommandContext):
         
-        await db.GetDatabase(id_, 'profile', {"uid": id_, "profile_background": "Normal", "profile_description": "I am a very mysterious person!"})
-        await db.SetDatabase(id_, 'profile', 'profile_background', choice)
+        edit_buttons = [
+            Button(label='Edit Background', custom_id='background', style=ButtonStyle.PRIMARY),
+            Button(label='Edit Description', custom_id='description', style=ButtonStyle.PRIMARY),
+        ]
         
-        await ctx.send(f'Successfully set profile background to: ``{choice}``', ephemeral = True)
+        await ctx.send('What do you want to edit?', components=edit_buttons, ephemeral = True)
         
-    @profile.subcommand(description='Change your profile description.')
-    @interactions.option(description='Set profile description, with a maximum of 100 characters.', max_length = 100, required = True)
-    async def set_description(self, ctx : interactions.CommandContext, description : str = "I am a very mysterious person!"):
-        id_ = int(ctx.author.id)
+        while True:
+            button_ctx : ComponentContext = await wait_for_component(self.client, edit_buttons)
+            
+            data = button_ctx.data.custom_id
+            
+            if (data == 'description'):
+                modal = Modal(
+                    title = 'Edit Profile Description',
+                    components=[TextInput(label = 'Description', style=TextStyleType.SHORT, custom_id='Among Us', max_length=100, placeholder='I am a very mysterious person!')],
+                    custom_id='ModalSus'
+                )
+                
+                await button_ctx.popup(modal)
+                
+            if (data == 'background'):
+                
+                profile_background_choices = [
+                    SelectOption(
+                        label='Normal',
+                        value='Normal'
+                    ),
+                    SelectOption(
+                        label='Red',
+                        value='Red'
+                    ),
+                    SelectOption(
+                        label='Blue',
+                        value='Blue'
+                    ),
+                    SelectOption(
+                        label='Green',
+                        value='Green'
+                    ),
+                    SelectOption(
+                        label='Yellow',
+                        value='Yellow'
+                    ),
+                    SelectOption(
+                        label='Pink',
+                        value='Pink'
+                    ),
+                ]
+                
+                menu = SelectMenu(custom_id='selection', options=profile_background_choices)
+                
+                await button_ctx.send('Select a profile background.', components=menu, ephemeral=True)
+                
+                SelectOption_ : ComponentContext = await wait_for_component(self.client, components = menu)
+                
+                id_ = int(ctx.author.id)
         
-        await db.GetDatabase(id_, 'profile', {"uid": id_, "profile_background": "Normal", "profile_description": "I am a very mysterious person!"})
-        await db.SetDatabase(id_, 'profile', 'profile_description', description)
-        
-        await ctx.send(f'Successfully set profile description to: ``{description}``', ephemeral = True)
-        
+                await db.GetDatabase(id_, 'profile', {"uid": id_, "profile_background": "Normal", "profile_description": "I am a very mysterious person!"})
+                await db.SetDatabase(id_, 'profile', 'profile_background', SelectOption_.data.values[0])
+                
+                await button_ctx.send(f'Successfully set profile background to: ``{SelectOption_.data.values[0]}``, use </profile view:8328932897324897> to view your changes.', ephemeral = True)
+
+            if (data == 'stamp'):
+                await button_ctx.send('Simply use </select_stamp:100> to select a stamp to use both in letters and your profile!', ephemeral = True)
+                
+            if (data == 'character'):
+                await button_ctx.send('Simply use </transmit hidden-profile:100> to select a character to use for Transmissions and your profile!', ephemeral = True)
+            
     @profile.subcommand(description='View a profile.')
-    @interactions.option(description='The user\'s profile to view.', required=True, type=interactions.OptionType.USER)
-    async def view(self, ctx : interactions.CommandContext, user : interactions.User):
+    @option(description='The user\'s profile to view.', required=True, type=OptionType.USER)
+    async def view(self, ctx : CommandContext, user : User):
         id_ = 0
 
         msg = ''
@@ -83,9 +113,96 @@ class Profile(interactions.Extension):
         
         await view.DrawBadges(id_, user_, user.avatar_url)
 
-        img_ = interactions.File('Badges/result.png')
+        img_ = File('Badges/result.png')
+        
+        edit_profile_button = Button(label='Edit Profile', custom_id='editprofile', style=ButtonStyle.SUCCESS)
 
-        await msg.edit('', files = img_)
+        await msg.edit('', files = img_, components=edit_profile_button)
+        
+        while True:
+            edit_profile : ComponentContext = await wait_for_component(self.client, edit_profile_button)
+            
+            if edit_profile.author.id != ctx.author.id:
+                edit_profile.send('Sorry! Only the person that owns this profile can edit it!', ephemeral=True)
+                return
+            
+            edit_buttons = [
+                Button(label='Edit Background', custom_id='background', style=ButtonStyle.PRIMARY),
+                Button(label='Edit Description', custom_id='description', style=ButtonStyle.PRIMARY),
+                Button(label='How to change stamp?', custom_id='stamp', style=ButtonStyle.PRIMARY),
+                Button(label='How to change character?', custom_id='character', style=ButtonStyle.PRIMARY),
+            ]
+        
+            await edit_profile.send('What do you want to edit?', components=edit_buttons, ephemeral = True)
+            
+            button_ctx : ComponentContext = await wait_for_component(self.client, edit_buttons)
+            
+            data = button_ctx.data.custom_id
+            
+            if (data == 'description'):
+                modal = Modal(
+                    title = 'Edit Profile Description',
+                    components=[TextInput(label = 'Description', style=TextStyleType.SHORT, custom_id='Among Us', max_length=100, placeholder='I am a very mysterious person!')],
+                    custom_id='ModalSus'
+                )
+                
+                await button_ctx.popup(modal)
+                
+            if (data == 'background'):
+                
+                profile_background_choices = [
+                    SelectOption(
+                        label='Normal',
+                        value='Normal'
+                    ),
+                    SelectOption(
+                        label='Red',
+                        value='Red'
+                    ),
+                    SelectOption(
+                        label='Blue',
+                        value='Blue'
+                    ),
+                    SelectOption(
+                        label='Green',
+                        value='Green'
+                    ),
+                    SelectOption(
+                        label='Yellow',
+                        value='Yellow'
+                    ),
+                    SelectOption(
+                        label='Pink',
+                        value='Pink'
+                    ),
+                ]
+                
+                menu = SelectMenu(custom_id='selection', options=profile_background_choices)
+                
+                await button_ctx.send('Select a profile background.', components=menu, ephemeral=True)
+                
+                SelectOption_ : ComponentContext = await wait_for_component(self.client, components = menu)
+                
+                id_ = int(ctx.author.id)
+        
+                await db.GetDatabase(id_, 'profile', {"uid": id_, "profile_background": "Normal", "profile_description": "I am a very mysterious person!"})
+                await db.SetDatabase(id_, 'profile', 'profile_background', SelectOption_.data.values[0])
+                
+                await button_ctx.send(f'Successfully set profile background to: ``{SelectOption_.data.values[0]}``, use </profile view:8328932897324897> to view your changes.', ephemeral = True)
+
+            if (data == 'stamp'):
+                await button_ctx.send('Simply use </select_stamp:100> to select a stamp to use both in letters and your profile!', ephemeral = True)
+                
+            if (data == 'character'):
+                await button_ctx.send('Simply use </transmit hidden-profile:100> to select a character to use for Transmissions and your profile!', ephemeral = True)
+    @extension_modal('ModalSus')
+    async def set_description(self, ctx : CommandContext, description : str):
+        id_ = int(ctx.author.id)
+        
+        await db.GetDatabase(id_, 'profile', {"uid": id_, "profile_background": "Normal", "profile_description": "I am a very mysterious person!"})
+        await db.SetDatabase(id_, 'profile', 'profile_description', description)
+        
+        await ctx.send(f'Successfully set profile description to: ``{description}``, use </profile view:8328932897324897> to view your changes.', ephemeral = True)
 
 def setup(client):
     Profile(client)
