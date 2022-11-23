d84d8d26414e20c203585300cabbfd21416bfa09
AxolPop <derfii2004@gmail.com>
2022-11-23 19:11:09 +0000

    Removal of the edit button in favor of command

--- profile.py
+++ profile.py
@@ -120,88 +120,8 @@ class Profile(Extension):
 
         img_ = File('Badges/result.png')
         
-        uid = uuid4()
-        
-        edit_profile_button = Button(label='Edit Profile', custom_id=f'editprofile{uid}', style=ButtonStyle.SUCCESS)
 
-        await msg.edit('', files = img_, components=edit_profile_button)
-        
-        while True:
-            edit_profile : ComponentContext = await wait_for_component(self.client, edit_profile_button)
-            
-            if edit_profile.author.id != user.id:
-                await edit_profile.send('Sorry! Only the person that owns this profile can edit it!', ephemeral=True)
-                return
-            
-            edit_buttons = [
-                Button(label='Edit Background', custom_id=f'background{uid}', style=ButtonStyle.PRIMARY),
-                Button(label='Edit Description', custom_id=f'description{uid}', style=ButtonStyle.PRIMARY),
-                Button(label='How to change stamp?', custom_id=f'stamp{uid}', style=ButtonStyle.PRIMARY),
-                Button(label='How to change character?', custom_id=f'character{uid}', style=ButtonStyle.PRIMARY),
-            ]
-        
-            await edit_profile.send('What do you want to edit?', components=edit_buttons, ephemeral = True)
-            
-            button_ctx : ComponentContext = await wait_for_component(self.client, edit_buttons)
-            
-            data = button_ctx.data.custom_id
-            
-            if (data == f'description{uid}'):
-                modal = Modal(
-                    title = 'Edit Profile Description',
-                    components=[TextInput(label = 'Description', style=TextStyleType.SHORT, custom_id='Among Us', max_length=100, placeholder='I am a very mysterious person!')],
-                    custom_id='ModalSus'
-                )
-                
-                await button_ctx.popup(modal)
-                
-            if (data == f'background{uid}'):
-                
-                profile_background_choices = [
-                    SelectOption(
-                        label='Normal',
-                        value='Normal'
-                    ),
-                    SelectOption(
-                        label='Red',
-                        value='Red'
-                    ),
-                    SelectOption(
-                        label='Blue',
-                        value='Blue'
-                    ),
-                    SelectOption(
-                        label='Green',
-                        value='Green'
-                    ),
-                    SelectOption(
-                        label='Yellow',
-                        value='Yellow'
-                    ),
-                    SelectOption(
-                        label='Pink',
-                        value='Pink'
-                    ),
-                ]
-                
-                menu = SelectMenu(custom_id='selection', options=profile_background_choices)
-                
-                await button_ctx.send('Select a profile background.', components=menu, ephemeral=True)
-                
-                SelectOption_ : ComponentContext = await wait_for_component(self.client, components = menu)
-                
-                id_ = int(ctx.author.id)
-        
-                await db.GetDatabase(id_, 'profile', {"uid": id_, "profile_background": "Normal", "profile_description": "I am a very mysterious person!"})
-                await db.SetDatabase(id_, 'profile', 'profile_background', SelectOption_.data.values[0])
-                
-                await button_ctx.send(f'Successfully set profile background to: ``{SelectOption_.data.values[0]}``, use </profile view:8328932897324897> to view your changes.', ephemeral = True)
-
-            if (data == f'stamp{uid}'):
-                await button_ctx.send('Simply use </select_stamp:100> to select a stamp to use both in letters and your profile!', ephemeral = True)
-                
-            if (data == f'character{uid}'):
-                await button_ctx.send('Simply use </transmit hidden-profile:100> to select a character to use for Transmissions and your profile!', ephemeral = True)
+        await msg.edit('', files = img_)
     @extension_modal('ModalSus')
     async def set_description(self, ctx : CommandContext, description : str):
         id_ = int(ctx.author.id)
